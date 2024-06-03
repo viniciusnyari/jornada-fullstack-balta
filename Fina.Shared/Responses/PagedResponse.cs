2a1284761a91f72cc02b8c94b621c980e7539f6e
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-06-03 15:37:06 -0300

    Finalizando o response e request

--- Fina.Shared/Responses/PagedResponse.cs
+++ Fina.Shared/Responses/PagedResponse.cs
@@ -5,16 +5,25 @@ namespace Fina.Shared.Responses;
 public class PagedResponse<TData> : Response<TData>
 {
     [JsonConstructor]
-    public PagedResponse(TData? data, 
+    public PagedResponse(
+        TData? data, 
         int totalCount,
         int currentPage = Configuration.DefaultPageNumber,
-        int pageSize = Configuration.DefaultPageSize) : base(data)
+        int pageSize = Configuration.DefaultPageSize) 
+        : base(data)
     {
         Data = data;
         TotalCount = totalCount;
         CurrentPage = currentPage;
         PageSize = pageSize;
     }
+    
+    public PagedResponse(
+        TData? data, 
+        int code = Configuration.DefaultStatusCodeSuccess,
+        string? message = null) 
+        : base(data, code, message)
+    {}
     
     public int CurrentPage { get; set; }
     public int TotalPage => (int)Math.Ceiling(TotalCount / (double)PageSize);
